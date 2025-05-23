19af32fa7a05d0a86ad26184b2bb6deac8eaaf02
Jonasz Lazar <104980334+jonasz-lazar-pwr@users.noreply.github.com>
2025-05-24 00:40:39 +0200

    feat (auth-service): Enhance JWT payload and correct registration route prefix
    Updates the JWT generation to include additional user details (email,
    first_name, last_name) in the token payload for better context in
    downstream services. Also corrects the route prefix for user registration.
    
    Key changes:
    - Modified `CustomJWTStrategy` in `api/core/auth.py` to add `email`, `first_name`, and `last_name` claims to the JWT payload.
    - Ensured `user.role.value` is used when adding the role claim.
    - Adjusted the prefix for `fastapi_users.get_register_router` in `api/main.py` from `/auth/register` to `/auth`, making the registration endpoint accessible at `/auth/register`.
    - Ensured JWT `token_audience` is a list and `lifetime_seconds` is an integer in the `get_jwt_strategy` factory.

--- auth-service/api/core/config.py
+++ auth-service/api/core/config.py
@@ -1,22 +1,27 @@
 # api/core/config.py
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
-    # DB config
     DB_HOST: str
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
     DB_PORT: str
     AUTH_SERVICE_DB_SCHEMA: str
-    # JWT config
+
     JWT_AUDIENCE: str
     JWT_ISSUER: str
     JWT_LIFETIME_SECONDS: int
     SECRET_KEY: str
 
-    class Config:
-        env_file = "../../../config/.env"
+    CORS_ALLOW_ORIGINS: str
+
+    model_config = SettingsConfigDict(
+        env_file = ".env",
+        env_file_encoding = 'utf-8',
+        extra = 'ignore'
+    )
 
 settings = Settings()
--- auth-service/api/core/middleware.py
+++ auth-service/api/core/middleware.py
@@ -2,12 +2,16 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from api.core.config import settings
+
 
 def add_middleware(app: FastAPI):
+    allow_origins = [origin.strip() for origin in settings.CORS_ALLOW_ORIGINS.split(",")]
+
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=False,
+        allow_origins=allow_origins,
+        allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
