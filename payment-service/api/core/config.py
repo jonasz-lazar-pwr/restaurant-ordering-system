e0bb3d4efdcef499e77ef7cf101dde78ee005c51
Jonasz Lazar <104980334+jonasz-lazar-pwr@users.noreply.github.com>
2025-06-06 14:28:23 +0200

    refactor: tiny changes in payment-service

--- payment-service/api/core/config.py
+++ payment-service/api/core/config.py
@@ -6,13 +6,16 @@ class Settings(BaseSettings):
     # RabbitMQ
     RABBITMQ_URL: str
     PAYMENT_QUEUE: str
+    ORDER_QUEUE: str
+    STAFF_QUEUE: str
+    NOTIFICATION_QUEUE: str
 
     # PayU
     # Change ID, POS_ID and SECRET after switching to proper shop
-    PAYU_CLIENT_ID = "490096"
-    PAYU_CLIENT_SECRET = "585ec418430275c1252a591b7ef07185"
-    PAYU_MERCHANT_POS_ID = "490096"
-    PAYU_SANDBOX_URL = "https://secure.snd.payu.com/"
+    PAYU_CLIENT_ID: str = "490096"
+    PAYU_CLIENT_SECRET: str = "585ec418430275c1252a591b7ef07185"
+    PAYU_MERCHANT_POS_ID: str = "490096"
+    PAYU_SANDBOX_URL: str = "https://secure.snd.payu.com/"
 
     CORS_ALLOW_ORIGINS: str
 
